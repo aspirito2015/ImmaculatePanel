47f1b09b5f705716dd89f98fa40e3f810d728d6f
aspirito2015 <aspirito2015@gmail.com>
2023-11-30 20:48:47 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js';
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-lite.js';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBNGvghLLvatMD7tuANyX6tyAtsEKPik5w",
+    authDomain: "immaculate-panel.firebaseapp.com",
+    projectId: "immaculate-panel",
+    storageBucket: "immaculate-panel.appspot.com",
+    messagingSenderId: "806020093352",
+    appId: "1:806020093352:web:cbf4f970ab2b6ed4c1b99d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCharacters(db) {
+    const charactersCol = collection(db, 'characters');
+    const charSnapshot = await getDocs(charactersCol);
+    const charList = charSnapshot.docs.map(doc => doc.data());
+    return charList;
+}
+
+alert(getCharacters(db));

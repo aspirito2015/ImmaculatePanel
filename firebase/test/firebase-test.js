ada243d5ea08ad8f2d3ce741c7531d8b947a0058
aspirito2015 <aspirito2015@gmail.com>
2023-12-02 20:04:10 -0500

    Moved firebase html & js files into firebase\test

--- firebase/test/firebase-test.js
+++ firebase/test/firebase-test.js
@@ -2,11 +2,14 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebas
 import {
     getFirestore,
     addDoc,
+    doc,
     collection,
     query,
     orderBy,
     onSnapshot,
-    Timestamp
+    Timestamp,
+    where,
+    getDocs
 } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
 
 // Your web app's Firebase configuration
@@ -21,45 +24,59 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const answersRef = collection(db, 'answers');
+const charactersRef = collection(db, 'characters');
 
 async function main() {
     let form = document.getElementById("leave-message");
-    let input = document.getElementById("message");
-    let checkboxes = document.getElementsByTagName("input");
     let ans_b = [];
-    let ans_c = new Array(9).fill("characters/oROwXqp4zlXB8ZbVFV1G");
-    for (var i=0; i<checkboxes.length; i++) {
-        ans_b[i] = checkboxes.checked;
-    } 
+    let ans_c = new Array(9).fill(doc(db, "characters", "oROwXqp4zlXB8ZbVFV1G"));
+
+    const q1 = query(charactersRef, where("name", "==", "test00"));
+    const querySnapshot = await getDocs(q1);
+
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+    });
+
+    for (let i=0; i<ans_c.length; i++) {
+        
+    }
+
     // Listen to the form submission
     form.addEventListener('submit', async e => {
         // Prevent the default form redirect
         e.preventDefault();
-        // Write a new message to the database collection "guestbook"
-        addDoc(collection(db, 'answers'), {
-            correct_arr: [true, true, true, true, true, true, true, true, true],
-            timestamp: Timestamp.now(),
-            name: "Anthony!",
+        // Get values of checkboxes
+        let checkboxes = document.getElementsByClassName("checkbox");
+        console.log(checkboxes);
+        for (var i=0; i<checkboxes.length; i++) {
+            ans_b[i] = checkboxes[i].checked;
+        }
+        console.log(ans_b);
+        // Write a new message to the database collection "answers"
+        addDoc(answersRef, {
+            correct_arr: ans_b,
+            char_arr: ans_c,
+            submitted: Timestamp.now()
         });
         // Return false to avoid redirect
         return false;
     });
 
-
     // Create query for messages
-    const q = query(collection(db, 'answers'));
-    onSnapshot(q, snaps => {
+    const q2 = query(answersRef);
+    onSnapshot(q2, snaps => {
         // Reset page
         guestbook.innerHTML = '';
         // Loop through documents in database
         snaps.forEach(doc => {
             // Create an HTML entry for each document and add it to the chat
             const entry = document.createElement('p');
-            entry.textContent = doc.data().correct_arr + ': ' + doc.data().datetime;
+            entry.textContent = doc.data().correct_arr + ': ' + doc.data().submitted.toDate();
             guestbook.appendChild(entry);
         });
     });
-
 }
 
 main();

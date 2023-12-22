eae36182159e3200e1d3fd33de17f4f20a2290c1
aspirito2015 <aspirito2015@gmail.com>
2023-12-21 21:11:09 -0500

    Add function to check validity of category combo
    check_cat_combo takes two category IDs and gets the number of characters with both categories in their cat_map

--- firebase.js
+++ firebase.js
@@ -3,7 +3,10 @@ import {
     getFirestore,
     doc,
     collection,
-    getDoc
+    getDoc,
+    query,
+    where,
+    getCountFromServer
 } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
 
 const firebaseConfig = {
@@ -30,4 +33,20 @@ export async function get_data_by_id(doc_id, coll_name) {
 
 export function get_ref(doc_id, coll_name) {
     return doc(db, coll_name, doc_id);
+}
+
+export async function check_cat_combo(cat_id_1, cat_id_2) {
+    const cat_ref_1 = doc(db, 'categories', cat_id_1);
+    const cat_ref_2 = doc(db, 'categories', cat_id_2);
+    const cat_q_1 = `cat_map.${cat_id_1}`;
+    const cat_q_2 = `cat_map.${cat_id_2}`;
+    const q = query(
+        charactersRef,
+        where(cat_q_1, '==', true),
+        where(cat_q_2, '==', true)
+    );
+    const snapshot = await getCountFromServer(q);
+    // using snapshot.data().count allows us to get the length of this...
+    // query's results without counting toward the read quota
+    console.log(`${cat_id_1} && ${cat_id_2} count: ${snapshot.data().count}`);
 }

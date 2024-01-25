1777075962b84428f430e56aa1544f24e0503a05
aspirito2015 <aspirito2015@gmail.com>
2024-01-25 07:00:48 -0500

    Fix path to image sources in header

--- components/header.js
+++ components/header.js
@@ -8,16 +8,16 @@ class Header extends HTMLElement {
         <div class="header">
             <div class="left" href="index.html">
                 <a href="#" style="text-decoration: none;">
-                    <img src="icon.png" width="32px" height="32px">
+                    <img src="../icon.png" width="32px" height="32px">
                     <h1>IMMACULATE PANEL</h1>
                 </a>
                 <h2>powered by</h2>
                 <h3>THE JANKIEST OF CODE</h3>
             </div>
             <div class="right">
-                <img src="icon_blank.png" width="32px" height="32px">
-                <img src="icon_blank.png" width="32px" height="32px">
-                <img src="icon_blank.png" width="32px" height="32px">
+                <img src="../icon_blank.png" width="32px" height="32px">
+                <img src="../icon_blank.png" width="32px" height="32px">
+                <img src="../icon_blank.png" width="32px" height="32px">
             </div>
         </div>
         `;

a12bc14704a2294eafcadbe5974ffbf6907a5b47
aspirito2015 <aspirito2015@gmail.com>
2024-02-07 21:37:01 -0500

    Update image and script src paths

--- components/header.js
+++ components/header.js
@@ -6,18 +6,18 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="header">
-            <div class="left" href="index.html">
-                <a href="#" style="text-decoration: none;">
-                    <img src="/MarvelGrid_HTML/images/icon.png" width="32px" height="32px">
+            <div class="left">
+                <a href="/" style="text-decoration: none;">
+                    <img src="images/icon.png" width="32px" height="32px">
                     <h1>IMMACULATE PANEL</h1>
                 </a>
                 <h2>powered by</h2>
                 <h3>THE JANKIEST OF CODE</h3>
             </div>
             <div class="right">
-                <img src="/MarvelGrid_HTML/images/icon_blank.png" width="32px" height="32px">
-                <img src="/MarvelGrid_HTML/images/icon_blank.png" width="32px" height="32px">
-                <img src="/MarvelGrid_HTML/images/icon_blank.png" width="32px" height="32px">
+                <img src="images/icon_blank.png" width="32px" height="32px">
+                <img src="images/icon_blank.png" width="32px" height="32px">
+                <img src="images/icon_blank.png" width="32px" height="32px">
             </div>
         </div>
         `;

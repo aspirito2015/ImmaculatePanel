6dec475171c10ac8acfc1be4cc071fc4ff3d77c6
aspirito2015 <aspirito2015@gmail.com>
2024-02-09 08:00:04 -0500

    Update sqliteQuerier.js

--- scripts/js/sqliteQuerier.js
+++ scripts/js/sqliteQuerier.js
@@ -2,16 +2,15 @@ export async function sqliteQuery(queryString) {
     // Fetch your SQLite database file (replace 'your_database_file.db' with the path to your SQLite database file)
     var response = await fetch('/data/ip.db');
     var buffer = await response.arrayBuffer();
-    var results;
     var url = 'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.1/sql-wasm.wasm';
     var result_json = {};
-
+    
     await initSqlJs({ locateFile: () => url }).then(SQL => {
         // Create a new database instance
         const db = new SQL.Database(new Uint8Array(buffer));
-
+        
         // Execute queries, e.g., SELECT statement
-        results = db.exec(queryString);
+        var results = db.exec(queryString);
         // Close the database connection
         db.close();
         if (results[0]!==undefined) {

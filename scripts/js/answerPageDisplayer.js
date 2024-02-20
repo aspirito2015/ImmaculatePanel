52937415130ce02a5fc87eef6be48ded7ef53d6b
aspirito2015 <aspirito2015@gmail.com>
2024-02-19 22:18:33 -0500

    Refactor answerPageDisplayer to use fewer queries

--- scripts/js/answerPageDisplayer.js
+++ scripts/js/answerPageDisplayer.js
@@ -7,16 +7,27 @@ async function main() {
     const categoryID_1 = urlParams.get('category1');
     const categoryID_2 = urlParams.get('category2');
     createHeading(categoryID_1, categoryID_2);
-    let q = `SELECT charID FROM edges WHERE catID=${categoryID_1} 
-        INTERSECT SELECT charID from edges WHERE catID=${categoryID_2}`;
+    let q = `
+        SELECT name, href, image, alias
+        FROM   characters
+        WHERE  charid IN (SELECT charid
+            FROM   edges
+            WHERE  catid = ${categoryID_1}
+            INTERSECT
+            SELECT charid
+            FROM   edges
+            WHERE  catid = ${categoryID_2})
+        ORDER BY appearances DESC;
+        `;
     let intersection = await sqliter.query_sqlite(q);
+    console.log(intersection);
     await arrToList(intersection, spitDiv);
 }
 
 async function createHeading(categoryID_1, categoryID_2) {
     let query = `SELECT catID, name, href FROM categories WHERE catID IN (${categoryID_1}, ${categoryID_2})`;
     let r = await sqliter.query_sqlite(query);
-    console.log(r);
+    // console.log(r);
     let s = `Characters who belong to both the<br>
         <a href="https://marvel.fandom.com${r[0].href}" target="_blank" 
         rel="noopener noreferrer">${r[0].name}</a> and 
@@ -31,23 +42,27 @@ async function arrToList(arr, parent) {
     container.setAttribute('class', "character-grid");
     parent.appendChild(container);
     for (let i = 0; i < arr.length; i++) {
-        createCharacterCell(arr[i].charID, container);
+        createCharacterCell(arr[i], container);
     }
 }
 
-async function createCharacterCell(characterID, parent) {
-    let q = `SELECT charID, name, href, image, alias 
-        FROM characters 
-        WHERE charID=${characterID}`;
-    let result = await sqliter.query_sqlite(q);
-    let data = result[0];
+async function createCharacterCell(charData, parent) {
+    // console.log(charData);
     let html_obj = document.createElement("div");
-    html_obj.setAttribute('onclick', `window.open('https://marvel.fandom.com${data.href}','_blank').focus();`);
+    html_obj.setAttribute('onclick', `window.open('https://marvel.fandom.com${charData.href}','_blank').focus();`);
     html_obj.setAttribute('style', 'cursor:pointer;');
-    let s = `<img src = "${data.image}">`
-    s += `<div>${data.name}`;
-    if (data.alias !== null) {
-        s += `<br><i>${data.alias}</i>`;
+    html_obj.setAttribute('class', 'drop-box');
+    let s = ``;
+    if (charData.image == "" || charData.image == null) {
+        s = `<img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" style="height: 100%;object-fit: cover;">`;
+    } else {
+        s = `<img src = "${charData.image}">`;
+    }
+    s += `<div>`;
+    s += `${charData.name}`;
+    if (charData.alias !== null) {
+        s += `<br>`;
+        s += `<i>${charData.alias}</i>`;
     }
     s += `</div>`;
     html_obj.innerHTML = s;

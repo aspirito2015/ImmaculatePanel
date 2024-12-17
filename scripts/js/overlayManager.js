89cf4e3698a3898db3e9987568a5d70f700500c9
Anthony <aspirito2015@gmail.com>
2024-12-16 22:42:55 -0500

    Add 'stop-propagation' class instead of adding listener piecemeal

--- scripts/js/overlayManager.js
+++ scripts/js/overlayManager.js
@@ -7,9 +7,13 @@ main();
 
 function main() {
     // Handle disabling overlays on click outside of the overlay
-    srch_bar.addEventListener("click", function (e) { e.stopPropagation(); });
-    document.getElementById('sum').addEventListener("click", function (e) {
-        e.stopPropagation();
+    let elements = document.getElementsByClassName("stop-propagation");
+    console.log(elements);
+    Array.from(elements).forEach(element => {
+        console.log(element);
+        element.addEventListener("click", function (e) {
+            e.stopPropagation();
+        });
     });
     document.getElementById('overlay').addEventListener("click", function() {
         search_off();
